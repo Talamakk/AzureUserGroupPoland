e15a4eac1caa610e608e0ddd5aeaf169cdc4ec31
Bartłomiej Dylik <bartlomiej.dylik@husqvarnagroup.com>
2025-02-18 20:20:35 +0100

    add testing script

--- FunctionApps/dev/Code/translateToHex/function_app.py
+++ FunctionApps/dev/Code/translateToHex/function_app.py
@@ -20,7 +20,7 @@ def public_function(req: func.HttpRequest) -> func.HttpResponse:
 
     if name:
         return func.HttpResponse(
-            f"Hello, {name}!\nIt's publicly exposed function here!",
+            f"Hello, {name}!\nIt's a publicly exposed function here!",
             status_code=200
         )
     else:
@@ -44,7 +44,31 @@ def secure_function(req: func.HttpRequest) -> func.HttpResponse:
 
     if name:
         return func.HttpResponse(
-            f"Hello, {name}!\nIt's secure function here!",
+            f"Hello, {name}!\nIt's a secure function here!",
+            status_code=200
+        )
+    else:
+        return func.HttpResponse(
+            "Please pass a name on the query string or in the request body",
+            status_code=400,
+        )
+
+@app.function_name(name="secure_function2")
+@app.route(route="secure_function2", auth_level=func.AuthLevel.FUNCTION)  # Requires API Key
+def secure_function(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info("Secure function processed a request.")
+
+    name = req.params.get("name")
+    if not name:
+        try:
+            req_body = req.get_json()
+            name = req_body.get("name")
+        except (ValueError, TypeError):
+            name = None
+
+    if name:
+        return func.HttpResponse(
+            f"Hello, {name}!\nIt's a secure function number 2 here!",
             status_code=200
         )
     else:
--- FunctionApps/dev/testing_script.ps1
+++ FunctionApps/dev/testing_script.ps1
@@ -5,13 +5,53 @@ function Call-FunctionApp {
     )
 
     $response = Invoke-RestMethod -Uri $apiUrl -Method Post -Body ($payload | ConvertTo-Json) -ContentType "application/json"
-    return $response
+    Write-Output $response
+    # return $response
 }
 
 # Call publicly exposed function
-$apiUrl = "https://fn-bdaug2025-gwc-dev.azurewebsites.net/api/hello"
+$apiUrl = "https://fn-bdaug2025-gwc-dev.azurewebsites.net/api/public_function?"
 $payload = @{
-    name = "xdbox"
+    name = "Bartek"
 }
-$result = Call-FunctionApp -apiUrl $apiUrl -payload $payload
-Write-Output "Function app response: $($result | ConvertTo-Json -Depth 10)"
+Call-FunctionApp -apiUrl $apiUrl -payload $payload
+
+# Call secure function (using function key)
+$apiUrl = "https://fn-bdaug2025-gwc-dev.azurewebsites.net/api//secure_function?"
+$functionKey = ""
+$hostKey = ""
+$payload = @{
+    name = "Bartek"
+}
+Call-FunctionApp -apiUrl "${apiUrl}code=${functionKey}" -payload $payload
+
+# Call secure function (using host key)
+$apiUrl = "https://fn-bdaug2025-gwc-dev.azurewebsites.net/api//secure_function2?"
+$functionKey = ""
+$hostKey = ""
+$payload = @{
+    name = "Bartek"
+}
+Call-FunctionApp -apiUrl "${apiUrl}code=${functionKey}" -payload $payload
+
+### Testing master key ###
+$masterKey = ""
+# Get host status
+Invoke-RestMethod -Uri "https://fn-bdaug2025-gwc-dev.azurewebsites.net/admin/host/status" `
+    -Headers @{ "x-functions-key" = "" } `
+    -Method Get
+
+# List all functions
+Invoke-RestMethod -Uri "https://fn-bdaug2025-gwc-dev.azurewebsites.net/admin/functions" `
+    -Headers @{ "x-functions-key" = "" } `
+    -Method Get
+
+# List all function keys
+Invoke-RestMethod -Uri "https://fn-bdaug2025-gwc-dev.azurewebsites.net/admin/functions/secure_function/keys" `
+    -Headers @{ "x-functions-key" = "" } `
+    -Method Get
+
+# Create a new function key
+...
+# Delete a function key
+...
